c520f67c9286fe79506772863c319d2a1dc91ed5
naveen-armedia <Naveen.Kerati@armedia.com>
2021-08-19 16:26:09 +0530

    removed setEnv.sh file

--- charts/ark-pentaho-ce/files/setenv.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-CATALINA_OPTS="-Xms550m -Xmx1024m  -Djava.rmi.server.hostname=localhost -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000"
-#export $CATALINA_OPTS
-#export CATALINA_OUT=/home/pentaho/log/pentaho/catalina.out
-#export CATALINA_TMPDIR=/home/pentaho/tmp/pentaho
-export JAVA_OPTS="-Djava.net.preferIPv4Stack=true"
