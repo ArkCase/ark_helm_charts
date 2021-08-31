b0d83838bb05d45bcc4731a8734938f1e57639d2
Fabrice Triboix <ftriboix@incise.co>
2021-08-31 14:53:55 +0100

    Fix a typo, make ArkCase protocol configurable

---
+++ charts/ark-snowbound/helm-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+helm install -n arkcase \
+        --set nameOverride=snowbound \
+        --set fullnameOverride=snowbound \
+        --set arkcase_protocol=https \
+        --set arkcase_host=13.126.43.123 \
+        --set arkcase_port=443 \
+        --set persistence.storageClass=gp2 \
+        snowbound .
