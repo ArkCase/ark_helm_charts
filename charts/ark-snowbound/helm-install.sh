647343d98907864b9dfc9c45faa2bc89ee2350c9
Fabrice Triboix <ftriboix@incise.co>
2021-09-03 15:20:13 +0100

    Finalize first draft of Snowbound Helm chart

--- charts/ark-snowbound/helm-install.sh
+++ charts/ark-snowbound/helm-install.sh
@@ -3,8 +3,7 @@
 helm upgrade --install -n arkcase \
         --set nameOverride=snowbound \
         --set fullnameOverride=snowbound \
-        --set arkcase_protocol=http \
-        --set arkcase_host=13.126.43.123 \
-        --set arkcase_port=443 \
+        --set snowbound_host_port=https://ec2-13-126-43-123.arkcase.com \
+        --set arkcase_host_port=https://ec2-13-126-43-123.arkcase.com \
         --set persistence.storageClass=gp2 \
         snowbound .
