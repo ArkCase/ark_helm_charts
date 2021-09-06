17f7259213601252e52bc23d00509f89abe6eeb5
Fabrice Triboix <ftriboix@incise.co>
2021-09-06 20:10:16 +0100

    Use efs-sc storage class when deploying to EKS

--- charts/ark-snowbound/helm-install.sh
+++ charts/ark-snowbound/helm-install.sh
@@ -5,5 +5,5 @@ helm upgrade --install -n arkcase \
         --set fullnameOverride=snowbound \
         --set snowbound_host_port=https://ec2-13-126-43-123.arkcase.com \
         --set arkcase_host_port=https://ec2-13-126-43-123.arkcase.com \
-        --set persistence.storageClass=gp2 \
+        --set persistence.storageClass=efs-sc \
         snowbound .
