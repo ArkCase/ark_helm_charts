d0f26934373635ca5d7999b8c906f7dac5e9fde2
Diego Rivera <diego.rivera@armedia.com>
2023-08-05 20:16:43 -0600

    Ordering improvements

--- src/pentaho/files/configmap/txt/config-post.sh
+++ src/pentaho/files/configmap/txt/config-post.sh
@@ -34,7 +34,6 @@ poll_url() {
 	say "Starting the polling cycle for [${URL}]..."
 	while true ; do
 		/usr/bin/curl -Lk -m 5 "${URL}" &>/dev/null && break
-		say "\tURL is not up yet at [${URL}]"
 		local NOW="$(date +%s)"
 		if [ $(( NOW - START )) -ge ${INIT_MAX_WAIT} ] ; then
 			err "Timed out waiting for the URL [${URL}] to come up"
@@ -47,7 +46,7 @@ poll_url() {
 			return 1
 		fi
 	done
-	say "The URL [${URL}] responded, continuing"
+	say "The URL [${URL}] has responded, continuing"
 	return 0
 }
 
@@ -58,6 +57,7 @@ set -euo pipefail
 [ -v BASE_DIR ] || BASE_DIR="/app"
 [ -v DATA_DIR ] || DATA_DIR="${BASE_DIR}/data"
 [ -v DWHS_DIR ] || DWHS_DIR="${DATA_DIR}/dw"
+[ -v FOIA_DIR ] || FOIA_DIR="${DWHS_DIR}/foia"
 [ -v LOGS_DIR ] || LOGS_DIR="${BASE_DIR}/logs"
 [ -v INIT_DIR ] || INIT_DIR="${BASE_DIR}/init"
 [ -v ADMIN_PORT ] || ADMIN_PORT="8080"
@@ -80,11 +80,6 @@ poll_url "${ADMIN_URL}" || fail "Cannot continue configuration if Pentaho is not
 
 [ -f "${RUN_MARKER}" ] || exit 0
 
-# Install the reports ... this can happen before the rest of the
-# FOIA/PDI stuff...
-say "Deploying reports"
-/usr/local/bin/install-reports
-
 # Now we move into the more time-sensitive stuff
 ARKCASE_CONNECTION_JSON="${PENTAHO_HOME}/.kettle/arkcase_connection.json"
 if [ -f "${ARKCASE_CONNECTION_JSON}" ] ; then
@@ -92,7 +87,6 @@ if [ -f "${ARKCASE_CONNECTION_JSON}" ] ; then
 	/usr/local/bin/add-pdi-connection "${ARKCASE_CONNECTION_JSON}"
 fi
 
-[ -v FOIA_DIR ] || FOIA_DIR="${DWHS_DIR}/foia"
 if [ -v FOIA_DIR ] && [ -d "${FOIA_DIR}" ] ; then
 
 	# Deploy the Mondrian schema
@@ -102,7 +96,21 @@ if [ -v FOIA_DIR ] && [ -d "${FOIA_DIR}" ] ; then
 	# Before we can run the dataminer first time, we MUST wait for
 	# ArkCase to come up ... if it doesn't, we're screwed
 	[ -v CORE_URL ] || CORE_URL="http://core:8080/arkcase/"
-	poll_url "${CORE_URL}" || fail "Cannot continue deployment if ArkCase is not online"
+	say "Launching the background poller for [${CORE_URL}]..."
+	coproc { poll_url "${CORE_URL}" ; }
+
+fi
+
+# Install the reports ... this *has* to happen last b/c the FOIA/PDI stuff
+# won't install cleanly if the rest of its dependencies aren't already covered
+say "Deploying reports"
+/usr/local/bin/install-reports
+
+if [ -v COPROC_PID ] ; then
+
+	# We only enter this if the polling coprocess was started, above
+	say "Joining the background poller for [${CORE_URL}]..."
+	wait ${COPROC_PID} || fail "Failed to wait for ArkCase to be online"
 
 	# TODO: This also needs to be a separate job/pod so it can be run periodically
 	say "Launching the first-time dataminer process"
