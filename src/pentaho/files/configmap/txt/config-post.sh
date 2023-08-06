8575cc6331ced6ba2af48c64ac748623dc20fe55
Diego Rivera <diego.rivera@armedia.com>
2023-08-05 18:57:24 -0600

    Reorganized init task order, wait for ArkCase FOIA

--- src/pentaho/files/configmap/txt/config-post.sh
+++ src/pentaho/files/configmap/txt/config-post.sh
@@ -28,6 +28,29 @@ cleanup() {
 	[ -z "${RUN_MARKER}" ] || rm -rf "${RUN_MARKER}" &>/dev/null
 }
 
+poll_url() {
+	local URL="${1}"
+	local START="$(date +%s)"
+	say "Starting the polling cycle for [${URL}]..."
+	while true ; do
+		/usr/bin/curl -Lk -m 5 "${URL}" &>/dev/null && break
+		say "\tURL is not up yet at [${URL}]"
+		local NOW="$(date +%s)"
+		if [ $(( NOW - START )) -ge ${INIT_MAX_WAIT} ] ; then
+			err "Timed out waiting for the URL [${URL}] to come up"
+			return 1
+		fi
+		# If sleep didn't succeed, it means it got signaled, which
+		# Means we need to stop what we're doing and puke out
+		if ! sleep ${INIT_POLL_SLEEP} ; then
+			err "Sleep interrupted, can't continue polling"
+			return 1
+		fi
+	done
+	say "The URL [${URL}] responded, continuing"
+	return 0
+}
+
 RUN_MARKER="${PENTAHO_HOME}/.initRan"
 trap cleanup EXIT
 set -euo pipefail
@@ -53,22 +76,16 @@ fi
 [[ "${INIT_MAX_WAIT}" =~ ^[1-9][0-9]*$ ]] || INIT_MAX_WAIT=900
 
 [ -v ADMIN_URL ] || ADMIN_URL="http://localhost:${ADMIN_PORT}/pentaho/"
-
-START="$(date +%s)"
-say "Starting the polling cycle"
-while true ; do
-	/usr/bin/curl -Lk -m 5 "${ADMIN_URL}" &>/dev/null && break
-	say "\tURL is not up yet at [${ADMIN_URL}]"
-	NOW="$(date +%s)"
-	[ $(( NOW - START )) -ge ${INIT_MAX_WAIT} ] && fail "Timed out waiting for the URL [${ADMIN_URL}] to come up"
-	# If sleep didn't succeed, it means it got signaled, which
-	# Means we need to stop what we're doing and puke out
-	sleep ${INIT_POLL_SLEEP} || fail "Sleep interrupted, can't continue polling"
-done
-say "The URL [${ADMIN_URL}] responded, continuing"
+poll_url "${ADMIN_URL}" || fail "Cannot continue configuration if Pentaho is not online"
 
 [ -f "${RUN_MARKER}" ] || exit 0
 
+# Install the reports ... this can happen before the rest of the
+# FOIA/PDI stuff...
+say "Deploying reports"
+/usr/local/bin/install-reports
+
+# Now we move into the more time-sensitive stuff
 ARKCASE_CONNECTION_JSON="${PENTAHO_HOME}/.kettle/arkcase_connection.json"
 if [ -f "${ARKCASE_CONNECTION_JSON}" ] ; then
 	say "Deploying the Kettle DB connection from [${ARKCASE_CONNECTION_JSON}]"
@@ -77,18 +94,19 @@ fi
 
 [ -v FOIA_DIR ] || FOIA_DIR="${DWHS_DIR}/foia"
 if [ -v FOIA_DIR ] && [ -d "${FOIA_DIR}" ] ; then
-	# TODO: Find a way to run this until AFTER the ArkCase schema is up and running
-	# TODO: This needs to be a separate job/pod so it can be run periodically
-	# say "Launching the dataminer process"
-	# /usr/local/bin/run-dataminer
 
 	# Deploy the Mondrian schema
 	say "Deploying the Mondrian schema"
 	/usr/local/bin/install-mondrian-schema "${FOIA_DIR}/mondrian_schema/foiaSchema1.4.xml"
-fi
 
-# Install the reports ...
-say "Deploying reports"
-/usr/local/bin/install-reports
+	# Before we can run the dataminer first time, we MUST wait for
+	# ArkCase to come up ... if it doesn't, we're screwed
+	[ -v CORE_URL ] || CORE_URL="http://core:8080/arkcase/"
+	poll_url "${CORE_URL}" || fail "Cannot continue deployment if ArkCase is not online"
+
+	# TODO: This also needs to be a separate job/pod so it can be run periodically
+	say "Launching the first-time dataminer process"
+	/usr/local/bin/run-dataminer
+fi
 
 exit 0
