763697311b230dd9484ce63a42823e6d266f29b6
Diego Rivera <diego.rivera@armedia.com>
2024-05-23 08:51:10 -0600

    More progress on the content container

--- src/app/charts/content/files/config/txt/config-post.sh
+++ src/app/charts/content/files/config/txt/config-post.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 [ -v DEBUG ] || DEBUG="false"
 case "${DEBUG,,}" in
@@ -8,7 +9,7 @@ esac
 
 timestamp()
 {
-	/usr/bin/date -Ins -u
+	date -Ins -u
 }
 
 say()
@@ -27,23 +28,16 @@ fail()
 	exit ${EXIT_CODE:-1}
 }
 
-cleanup()
-{
-	[ -v RUN_MARKER ] || RUN_MARKER=""
-	[ -z "${RUN_MARKER}" ] || rm -rf "${RUN_MARKER}" &>/dev/null
-}
-
-RUN_MARKER="${HOME}/.initRan"
-trap cleanup EXIT
-set -euo pipefail
-
-[ -v BASE_DIR ] || BASE_DIR="/app"
-[ -v DATA_DIR ] || DATA_DIR="${BASE_DIR}/data"
-[ -v LOGS_DIR ] || LOGS_DIR="${DATA_DIR}"
+[ -v BASE_DIR ] || BASE_DIR=""
+[ -n "${BASE_DIR}" ] || BASE_DIR="/app"
+[ -v DATA_DIR ] || DATA_DIR=""
+[ -n "${DATA_DIR}" ] || DATA_DIR="${BASE_DIR}/data"
+[ -v LOGS_DIR ] || LOGS_DIR=""
+[ -n "${LOGS_DIR}" ] || LOGS_DIR="${DATA_DIR}"
 
 if [ -d "${LOGS_DIR}" ] ; then
 	LOG_FILE="${LOGS_DIR}/config-post.log"
-	exec >> >(/usr/bin/tee -a "${LOG_FILE}")
+	exec >> >(tee -a "${LOG_FILE}")
 	exec 2>&1
 	say "Logs redirected to [${LOG_FILE}]"
 fi
@@ -62,14 +56,14 @@ PROBE_URL="${ADMIN_URL}/minio/health/ready"
 START="$(date +%s)"
 say "Starting the polling cycle"
 while true ; do
-	/usr/bin/curl -fsSL -m 5 "${PROBE_URL}" &>/dev/null && break
+	curl -fsSL -m 5 "${PROBE_URL}" &>/dev/null && break
 	NOW="$(date +%s)"
 	[ $(( NOW - START )) -ge ${INIT_MAX_WAIT} ] && fail "Timed out waiting for the URL [${PROBE_URL}] to come up"
 	# If sleep didn't succeed, it means it got signaled, which
 	# Means we need to stop what we're doing and puke out
 	sleep ${INIT_POLL_SLEEP} || fail "Sleep interrupted, can't continue polling"
 done
-OUT="$(/usr/bin/curl -fL -m 5 "${PROBE_URL}" 2>&1)" || fail "Unable to access the URL [${PROBE_URL}] (rc=${?}): ${OUT}"
+OUT="$(curl -fL -m 5 "${PROBE_URL}" 2>&1)" || fail "Unable to access the URL [${PROBE_URL}] (rc=${?}): ${OUT}"
 
 say "The URL [${PROBE_URL}] responded, continuing"
 
@@ -78,16 +72,21 @@ INSTANCE="local"
 # Add the configuration for MC control
 mcli config host add "${INSTANCE}" "${ADMIN_URL}" "${MINIO_ROOT_USER}" "${MINIO_ROOT_PASSWORD}"
 
-POLICIES=("consoleAdmin" "readwrite")
-for POLICY in "${POLICIES[@]}" ; do
-	# If the policy is already set, keep going
-	mcli idp ldap policy entities --policy "${POLICY}" "${INSTANCE}" |& grep -qi "${LDAP_ADMIN_GROUP}" && continue
+# We ALWAYS "create" the user accounts b/c we want to always rotate
+# the passwords to whatever they are at the moment of bootup
+PREFIX="MINIO_SERVICE"
+for T in "RO" "RW" ; do
+	POLICY="readonly"
+	[ "${T}" == "RW" ] && POLICY="readwrite"
+	U="${PREFIX}_${T}_USER"
+	P="${PREFIX}_${T}_PASSWORD"
+	[ -v "${U}" ] || continue
+	[ -v "${P}" ] || continue
+	[ -n "${!U}" ] && [ -n "${!P}" ] || continue
 
-	# Apply the policy
-	mcli idp ldap policy attach "${INSTANCE}" "${POLICY}" --group="${LDAP_ADMIN_GROUP}" || fail "Unable to set the [${POLICY}] policy for [${LDAP_ADMIN_GROUP}]"
+	say "Creating the ${T} service account with username [${!U}] (policy = ${POLICY})..."
+	mcli admin user add "${INSTANCE}" "${!U}" "${!P}" || fail "Failed to create the user account for [${!U}]..."
+	mcli admin policy attach "${INSTANCE}" "${POLICY}" --user "${!U}" || fail "Failed to attach the ${POLICY} policy to user [${!U}]..."
 done
-
-# Create the "arkcase" user if they don't exist
-# Grant the necessary (admin, for now) access to the "arkcase" user
 
 exit 0
