435b506c60516ee0f951ca9dec782a838c0e5760
Diego Rivera <diego.rivera@armedia.com>
2026-04-07 14:36:07 -0600

    Add a bit of a wait cycle for the probes
    A cleaner solve would be an init-container that initializes the SSL stuff, much like is done for HAProxy, but this is quicker for now. This protects against probes attempting to initialize the Keystore and whatnot, since the flock call in the acme-init script appears to not be working

--- src/app/charts/zookeeper/files/config/txt/zookeeper-env.sh
+++ src/app/charts/zookeeper/files/config/txt/zookeeper-env.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
 . /.functions
+
+#
+# Wait until the SSL stuff is ready!
+#
+START="$(date -u +%s)"
+MAX="120"
+MAX_STR="$(secs_to_timestr "${MAX}")"
+FIRST="true"
+while true ; do
+	is_file "${SSL_DIR}/.env" && break
+	as_boolean "${FIRST}" && sleeping "The SSL configuration isn't ready yet, will wait up to ${MAX_STR}"
+	FIRST="false"
+	sleep 0.1 || fail "Sleep interrupted!"
+	NOW="$(date -u +%s)"
+	[ $(( NOW - START )) -lt ${MAX} ] || fail "The SSL configuration didn't come up in time!"
+done
+
+as_boolean "${FIRST}" || ok "SSL configuration completed!"
+
 init_ssl
